ca8ad7d3010aea45affc86010b8a7343651bd496
crzydg <42@101010.li>
2023-02-01 11:41:32 +0100

    Added $releasever and $basearch as variables - see if it works

--- setup_repo.sh
+++ setup_repo.sh
@@ -21,9 +21,9 @@ for IP in "${IPs[@]}"; do
 
     # Check the network and deploy custom yum repository
     if [[ $NETWORK == "backend" ]]; then
-      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://192.168.0.100/repo\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
+      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://192.168.0.100/repo/rhel/$releasever/$basearch\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
     elif [[ $NETWORK == "DMZ" ]]; then
-      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://172.28.0.100/repo\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
+      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://172.28.0.100/repo/rhel/$releasever/$basearch\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
     fi
   fi
 done
