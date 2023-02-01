8a6540b860e4a8d052051c279fcb7eebec7a5fe3
gregor <gregor@penguin>
2023-02-01 10:53:53 +0100

    added to quick'n dirty deploy a new repo to a set of servers

---
+++ setup_repo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# IP addresses to iterate over
+IPs=(192.168.0.1 192.168.0.2 194.31.0.1 194.31.0.2)
+
+# Loop over the IP addresses
+for IP in "${IPs[@]}"; do
+
+  # Check the Linux distribution
+  DISTRO=$(ssh root@$IP 'cat /etc/*-release' | grep "ID=" | awk -F "=" '{print $2}')
+
+  # Check the network
+  if [[ $IP == 192* ]]; then
+    NETWORK="backend"
+  elif [[ $IP == 194* ]]; then
+    NETWORK="DMZ"
+  fi
+
+  # Check if the distribution is RHEL
+  if [[ $DISTRO == "rhel" ]]; then
+
+    # Check the network and deploy custom yum repository
+    if [[ $NETWORK == "backend" ]]; then
+      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://192.168.0.100/repo\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
+    elif [[ $NETWORK == "DMZ" ]]; then
+      ssh root@$IP 'echo "[customrepo]\nname=Custom Repository\nbaseurl=http://172.28.0.100/repo\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/customrepo.repo'
+    fi
+  fi
+done
+
