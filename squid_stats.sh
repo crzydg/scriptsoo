a97e06d84b60eb7f8f866964e7a197641dc09acc
crzydg <42@101010.li>
2023-03-24 14:00:31 +0200

    top 100 accessed URLs, top 100 requestors (client IPs), and top 100 accessed websites (based on the domain)

---
+++ squid_stats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the access.log file exists
+if [ ! -f /var/log/squid/access.log ]; then
+    echo "Squid access.log file not found at /var/log/squid/access.log"
+    echo "Please make sure the file exists and the path is correct."
+    exit 1
+fi
+
+# Generate a report of the top 100 accessed URLs
+echo "Top 100 Accessed URLs:"
+awk '{print $7}' /var/log/squid/access.log | sort | uniq -c | sort -nr | head -n 100
+echo ""
+
+# Generate a report of the top 100 requestors
+echo "Top 100 Requestors:"
+awk '{print $3}' /var/log/squid/access.log | sort | uniq -c | sort -nr | head -n 100
+echo ""
+
+# Generate a report of the top 100 accessed websites
+echo "Top 100 Accessed Websites:"
+awk -F/ '{print $1"//"$3}' /var/log/squid/access.log | sort | uniq -c | sort -nr | head -n 100
+
+
