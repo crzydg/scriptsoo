2c1c00967c98cab878cf3f312d0fa91dee72a21c
crzydg <42@101010.li>
2024-02-05 11:04:40 +0100

    minor syntax change.

--- apache_restart.sh
+++ apache_restart.sh
@@ -3,7 +3,6 @@
 # Definiere Grenzwerte
 CPU_LIMIT=80  # CPU-Grenze in Prozent
 MEM_LIMIT=500 # Speichergrenze in MB
-
 # Apache Prozess-Name
 APACHE_PROCESS_NAME='apache2' # oder 'httpd' auf RHEL Systemen
 
@@ -12,11 +11,11 @@ APACHE_CPU_USAGE=$(ps -C $APACHE_PROCESS_NAME -o %cpu --no-headers | awk '{sum+=
 APACHE_MEM_USAGE=$(ps -C $APACHE_PROCESS_NAME -o rss --no-headers | awk '{sum+=$1} END {print int(sum/1024)}') # Umwandlung in MB
 
 # Prüfe, ob Grenzwerte überschritten werden
-# use, if bc is not installed:
+# use, if bc is not installed - ist nicht ganz so genau
 #if [ "$APACHE_CPU_USAGE" -gt "$CPU_LIMIT" ] || [ "$APACHE_MEM_USAGE" -gt "$MEM_LIMIT" ]; then
 if (( $(echo "$APACHE_CPU_USAGE > $CPU_LIMIT" | bc -l) )) || [ "$APACHE_MEM_USAGE" -gt "$MEM_LIMIT" ]; then
     echo "Apache über Grenzwert. Neustart wird ausgeführt."
-    systemctl restart apache2 # Verwenden Sie 'service apache2 restart' auf älteren Systemen
+    systemctl restart $APACHE_PROCESS_NAME 
 else
     echo "Apache innerhalb der Grenzwerte."
 fi
